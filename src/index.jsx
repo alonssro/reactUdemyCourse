4d06de04e69d779349ebed2aea7a833e2e5d4c95
alonssro <rodriguezalonsod@outlook.com>
2018-11-04 00:45:57 -0600

    Handling null props

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SearchBar from "./components/search_bar.jsx";
 import YTSearch from "youtube-api-search";
 import config from "../config";
-
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_details";
 const API_KEY = config.API_YOUTUBE_KEY;
-console.log(API_KEY);
-YTSearch({ key: API_KEY, term: "surfboards" }, data => console.log(data));
 
-// Create a new component. This components should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+      this.setState({ videos, selectedVideo: videos[0] });
+    });
+  }
 
-//Take this commponent's generated HTML and put it on the page
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onSelectedVideo={selectedVideo => {
+            this.setState({ selectedVideo });
+          }}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
