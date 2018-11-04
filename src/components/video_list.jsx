15f9685350a8e4212e802710917c2b3a929eb3fc
alonssro <rodriguezalonsod@outlook.com>
2018-11-04 00:59:08 -0600

    Added css style to our app

--- src/components/video_list.jsx
+++ src/components/video_list.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import VideoListItem from "./video_list_item";
 const VideoList = props => {
   const VideoItems = props.videos.map(video => {
-    console.log(video);
     return (
       <VideoListItem
         onVideoSelect={props.onVideoSelect}
