6678de176cd7d63e8f5d6af36d98232a0ccc4892
alonssro <rodriguezalonsod@outlook.com>
2018-11-03 22:56:58 -0600

    Change to a class-based component & initialize the concept of state

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SearchBar from "./components/search_bar.jsx";
 
 const API_KEY = process.env.API_KEY_YOUTUBE;
 
 // Create a new component. This components should produce HTML
 const App = () => {
-  return <div> Hi </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 //Take this commponent's generated HTML and put it on the page
